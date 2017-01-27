ef1374eff4ee11897589d80311e0c1525f533918
victorm-fj <victorm.floresj@gmail.com>
2017-01-27 17:51:01 -0600

    Add sqlite-storage, foodsDB, link library, and some queries

--- app/App.js
+++ app/App.js
@@ -4,6 +4,7 @@ import {
   Text,
   View,
 } from 'react-native';
+import SQLite from 'react-native-sqlite-storage';
 
 const styles = StyleSheet.create({
   container: {
@@ -25,7 +26,40 @@ const styles = StyleSheet.create({
 });
 
 class App extends Component {
+  // error callback function for openDatabase operation
+  errorCB = (err) => {
+    console.log(`SQL Error: ${err}`);
+  }
+
+  // success callback function for openDatabase operation
+  successCB = () => {
+    console.log('SQL executed fine');
+  }
+
   render() {
+    const db = SQLite.openDatabase({
+      name: 'foodsDB', readOnly: true, createFromLocation: 1,
+    }, this.successCB, this.errorCB);
+
+    // 'food' being table name, item(0) is column names
+    // so we set limit 2 to retrieve the first food values
+    db.executeSql('SELECT * FROM food LIMIT 2', [],
+    (results) => {
+      console.log(results.rows.item(1));
+    },
+    (error) => {
+      console.log(error);
+    });
+
+    // sql query that counts db rows
+    db.executeSql('SELECT count(*) AS mycount FROM food', [],
+    (rs) => {
+      console.log(`Record count (expected to be 8791): ${rs.rows.item(0).mycount}`);
+    },
+    (error) => {
+      console.log(`SELECT SQL statement ERROR: ${error.message}`);
+    });
+
     return (
       <View style={styles.container}>
         <Text style={styles.title}>
